268c97576baba66732f0466a2d7ddd1f45aa2bac
zihao-cpu <68882271+zihao-cpu@users.noreply.github.com>
2025-02-22 14:44:44 +0800

    Update 统计估计及统计特征.m

--- MATLAB与概率论/统计估计及统计特征.m
+++ MATLAB与概率论/统计估计及统计特征.m
@@ -6,3 +6,14 @@ h=normplot(x);
 qqplot(x);
 qqplot(x,y); 
 
+%%4.3参数点估计
+%极大似然估计
+[muhat,sigmahat,muci,sigmaci]=normfit(X,0.01); % 99%的置信区间
+[phat,pci]=mle(x,'distribution',dist,'alpha',a);
+
+%%4.4区间估计
+X;
+mu=mean(X);
+muLOWER=mean(X)-tinv(0.95,size(X)-1)*sqrt(var(x)/size(X));
+
+
